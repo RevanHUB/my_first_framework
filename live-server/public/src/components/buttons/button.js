ea78368a8030834d57311d3550d0055104fae005
David <davidasir2020@gmail.com>
2022-11-15 12:44:20 +0000

    chore:fixed mode

--- live-server/public/src/components/buttons/button.js
+++ live-server/public/src/components/buttons/button.js
@@ -2,11 +2,12 @@
 
 document.getElementById("theme").addEventListener('click', () => {
     if (document.getElementById("theme").value === 'light') {
-        location.assign('index.php?routes=home&theme=dark');
+        location.assign('?routes=home&theme=dark');
         console.log('changing to dark');
     }
     if (document.getElementById("theme").value === 'dark') {
-        location.assign('index.php?routes=home&theme=light');
+
+        location.assign('?routes=home&theme=light');
         console.log('changing to light');
     }
 } )
